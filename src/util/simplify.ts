5c2c425e4dd3cf6e422eb2a53f6120a4f99e5894
Elyse <kevincristian@outlook.com>
2022-01-09 16:49:52 -0600

    Simplify: Improve name variables

--- src/util/simplify.ts
+++ src/util/simplify.ts
@@ -3,68 +3,72 @@ import nodejieba from 'nodejieba';
 import { s2tDict, t2sDict } from 'util/pinyinDict';
 import { getPinyinSegments } from 'util/segmentation';
 
-const simplify = (text: string): string => {
-    return getPinyinSegments(text).map((x) => {
-        if (x in t2sDict) {
-            return t2sDict[x];
-        }
-        return x;
-    }).join('');
-};
+const SpecialCharacters = new Set(['只', '喂', '面', '发', '干']);
 
-const specialChars = new Set(['只', '喂', '面', '发', '干']);
+const traditionalizeSpecialCharacter = (character: string, textBeforeCharacter: string[], textAfterText: string[]): string => {
+    if (character.length !== 1) {
+        throw Error('TraditionalizeSpecialCharacter only takes a single character');
+    }
 
-const traditionalizeSpecialChar = (char: string, beforeText: string[], afterText: string[]) => {
-    let prev; let after; let
-        last2;
-    switch (char) {
+    let previousCharacter: string;
+    let followingCharacter: string;
+    let lastTwoCharacters: string;
+
+    switch (character) {
     case '只':
-        prev = nodejieba.tag(beforeText.join('')).slice(-1)[0];
-        after = nodejieba.tag(afterText.join(''))[0];
-        if (prev && prev.tag === 'm') {
+        previousCharacter = nodejieba.tag(textBeforeCharacter.join('')).slice(-1)[0].tag;
+        followingCharacter = nodejieba.tag(textAfterText.join(''))[0].tag;
+        if (previousCharacter === 'm') {
             return '隻';
         }
-        if (after && after.tag === 'n') {
+        if (followingCharacter === 'n') {
             return '隻';
         }
         return '只';
     case '喂':
-        after = nodejieba.tag(afterText.join(''))[0];
-        if (after && after.tag === 'n') {
+        followingCharacter = nodejieba.tag(textAfterText.join(''))[0].tag;
+        if (followingCharacter === 'n') {
             return '餵';
         }
         return '喂';
     case '面':
-        prev = nodejieba.tag(beforeText.join('')).slice(-1)[0];
-        if (prev && prev.tag === 'v') {
+        previousCharacter = nodejieba.tag(textBeforeCharacter.join('')).slice(-1)[0].tag;
+        if (previousCharacter === 'v') {
             return '麵';
         }
         return '面';
     case '发':
-        last2 = beforeText.join('').slice(-2);
-        if (last2.includes('理') || last2.includes('头')) {
+        lastTwoCharacters = textBeforeCharacter.join('').slice(-2);
+        if (lastTwoCharacters.includes('理') || lastTwoCharacters.includes('头')) {
             return '髮';
         }
         return '發';
     case '干':
-        last2 = beforeText.join('').slice(-2);
-        if (last2.includes('变')) {
+        lastTwoCharacters = textBeforeCharacter.join('').slice(-2);
+        if (lastTwoCharacters.includes('变')) {
             return '乾';
         }
         return '幹';
     default:
-        return char in s2tDict ? s2tDict[char] : char;
+        return character in s2tDict ? s2tDict[character] : character;
     }
 };
 
+const simplify = (text: string): string => {
+    const pinyinSegments = getPinyinSegments(text).map((pinyinSegment) => {
+        return pinyinSegment in t2sDict ? t2sDict[pinyinSegment] : pinyinSegment;
+    });
+    return pinyinSegments.join('');
+};
+
 const traditionalize = (text: string): string => {
-    return getPinyinSegments(text).map((x, i, segments) => {
-        if (specialChars.has(x)) {
-            return traditionalizeSpecialChar(x, segments.slice(0, i), segments.slice(i + 1));
+    const pinyinSegments = getPinyinSegments(text).map((pinyinSegment, i, segments) => {
+        if (SpecialCharacters.has(pinyinSegment)) {
+            return traditionalizeSpecialCharacter(pinyinSegment, segments.slice(0, i), segments.slice(i + 1));
         }
-        if (x in s2tDict) return s2tDict[x];
-        return x;
-    }).join('');
+        return pinyinSegment in s2tDict ? s2tDict[pinyinSegment] : pinyinSegment;
+    });
+    return pinyinSegments.join('');
 };
 
 export { simplify, traditionalize };
