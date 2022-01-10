acd87f08690e1b2f44d5aaf38b77fea9da27e816
Elyse <kevincristian@outlook.com>
2022-01-09 23:19:00 -0600

    Add package's public interface

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import {TagResult} from 'nodejieba';
 
 declare namespace HanziTools {
     interface PinyinDetailed {
@@ -9,4 +10,14 @@ declare namespace HanziTools {
     }
 }
 
-export { HanziTools };
+function pinyinify(text: string, isDetailed: boolean = false): string | HanziTools.PinyinDetailed;
+
+function getPinyinSegments(text: string): string[];
+
+function simplify(text: string): string;
+
+function traditionalize(text: string): string;
+
+function getTextTags(text: string): TagResult[];
+
+export {pinyinify, getPinyinSegments, simplify, traditionalize, getTextTags, HanziTools};
